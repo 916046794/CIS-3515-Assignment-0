44e886174b4266ab2816a3cc69b251362d425bdb
916046794 <tuo74327@temple.edu>
2025-09-07 21:45:21 -0400

    Fixed bugs+added Main

--- Shape.kt
+++ Shape.kt
@@ -1,10 +1,14 @@
 abstract class Shape (_name : String) : Dimensionable{
     //implements Dimensionable interface, has a constant named name that's public
-
     var name = _name
-    fun getArea(): Double {
+
+    open fun getArea(): Double {
+        //it said to make this open (shrug)
+        //weirdhow it prints kotlin.Unit right before it but I think bc of the typecast
         return 0.0;
     }
 
-    override fun printDimensions(){}
+    override fun printDimensions(){
+        print("")
+    }
 }
---
+++ out/production/CIS-3515-Assignment-0/README.md
@@ -0,0 +1,2 @@
+# CIS-3515-Assignment-0
+An optional assignment to practice using OOP and Kotlin
